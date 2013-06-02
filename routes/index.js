4c2b7048d5400706decf48fc77113d4a7e610092
EragonJ <eragonj@eragonj.me>
2013-06-02 11:18:36 +0800

    update route (missing req, res var)

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,10 @@ exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
 
-exports.top100 = function() {
+exports.top100 = function(req, res) {
   res.render('top100', { title : 'Express'});
 };
 
-exports.exam = function() {
+exports.exam = function(req, res) {
   res.render('exam', { title : 'Express'});
 };
