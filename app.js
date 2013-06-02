3ba43b82adfa0a44e3b3d2b54cc68d3ca73d07db
Isken Huang <kensheep@gmail.com>
2013-06-02 10:17:30 +0800

    update structure to express

--- app.js
+++ app.js
@@ -5,11 +5,21 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
-
-var app = express();
+  , path = require('path')
+  , app = express()
+  , allowCrossDomain = function(req, res, next){
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With');
+      next();
+    }
+  , handleOptionsMethod = function(req, res, next){
+      if( req.method == 'OPTIONS'){
+        return res.send(200);
+      }
+      next();
+    };
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -23,14 +33,18 @@ app.configure(function(){
   app.use(express.session());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+
+  // for cors
+  app.use(allowCrossDomain);
+  app.use(handleOptionsMethod);
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// app.configure('development', function(){
+//   app.use(express.errorHandler());
+// });
 
+// router here
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
