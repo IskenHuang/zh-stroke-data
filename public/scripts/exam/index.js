609cb7ec5f2f5a09613073b7374a1190a2d7a887
EragonJ <eragonj@eragonj.me>
2013-06-02 13:08:10 +0800

    add exam logic & some mockup data

--- public/scripts/exam/index.js
+++ public/scripts/exam/index.js
@@ -6,7 +6,22 @@ $(function() {
     // random out the exam id first
 
     exam.fetch( examId , function(examData) {
+
+        $(".submit-exam").removeAttr("disabled");
+
         // examData.title
         // examData.content
+    });
+
+    $(".submit-exam").on("click", function() {
+
+        var ans = $(".user-input").val();
+
+        if ( exam.checkAnswer(ans)) {
+            alert('y');
+        }
+        else {
+            alert('x');
+        }
     });
 });
